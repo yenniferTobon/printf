0fcb26fadc3e3cae26c2396eff58ac2e203738ef
yenniferTobon <ymtobony@unal.edu.co>
2019-07-29 22:02:37 +0000

    _printf - % last position

--- _printf.c
+++ _printf.c
@@ -21,11 +21,19 @@ int _printf(const char *format, ...)
 	{
 		if (*(format + i) == '%')
 		{
+			if(*(format + (i + 1)) == '\0')
+			{
+				if (i != 0)
+					return (-1);
+				else
+					return (-1);
+			}
 			switch (*(format + (i + 1)))
 			{
 				case 'c':
 					_putchar((char)va_arg(argument, int));
 					size++;
+					i = i + 2;
 					break;
 				case 's':
 					str = (va_arg(argument, char*));
@@ -36,13 +44,14 @@ int _printf(const char *format, ...)
 					}
 					_puts(str);
 					size = size + _strlen(str);
+					i = i + 2;
 					break;
 				case '%':
 					_putchar('%');
 					size++;
+					i = i + 2;
 					break;
 			}
-			i = i + 2;
 		}
 		_putchar(format[i]);
 		size = size + 1;
