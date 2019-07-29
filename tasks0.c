54bc7fd69479b597b85c37f28d14ce6a7309ca96
yenniferTobon <ymtobony@unal.edu.co>
2019-07-29 16:01:22 +0000

    case %

--- tasks0.c
+++ tasks0.c
@@ -2,44 +2,57 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * _printf - produces output according to a format
+ *  @format: list arguments
+ * Return: int
+ **/
 int _printf(const char *format, ...)
 {
 	va_list argument;
-        int i = 0, size = 0, j = 0;
+	int i = 0, size = 0, j = 0;
 	char *str;
 
-        while (format == NULL)
-        {
-                return (0);
-        }
+	while (format == NULL)
+		return (0);
 
 	va_start(argument, format);
-        for (i = 0; format[i] != '\0'; i++)
-        {
-                size = size + 1;
-
-              /**  if (*(format + i) == '\n')
-                {
-                        _putchar('\n');
-                }*/
-		if (*(format + i) == '%' && (*(format + (i + 1)) == 'c'))
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (*(format + i) == '%')
 		{
-			_putchar((char)va_arg(argument, int));
-			i = i + 2;
-		}
-		if (*(format + i) == '%' && (*(format + (i + 1)) == 's'))
-		{
-			str = (va_arg(argument, char*));
-			while (str[j] != '\0' && str != NULL)
+			switch (*(format + (i + 1)))
 			{
-				_putchar(str[j]);
-				j++;
+				case 'c':
+					_putchar((char)va_arg(argument, int));
+					size++;
+					break;
+				case 's':
+					str = (va_arg(argument, char*));
+					if (str == NULL)
+					{
+						printf("(nil)");
+						break;
+					}
+					j = 0;
+					while (str[j] != '\0' && str != NULL)
+					{
+						_putchar(str[j]);
+						size++;
+						j++;
+					}
+					break;
+				case '%':
+					_putchar('%');
+					size++;
+					break;
 			}
 			i = i + 2;
 		}
 		_putchar(format[i]);
+		size = size + 1;
 	}
 	va_end(argument);
-        return size;
+        return (size);
 }
 
